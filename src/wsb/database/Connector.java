b02ff53f0452c15b5d6a9183001a13dfd86d1a7e
Kacper Warda <kacper.warda@logintegra.com>
2020-06-14 16:43:47 +0200

    task: db connector

--- src/wsb/database/Connector.java
+++ src/wsb/database/Connector.java
@@ -1,20 +1,24 @@
 package wsb.database;
 
+import wsb.Configuration;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 public class Connector {
     static final String DB_URL = "jdbc:postgresql://localhost:5432/oop";
-    static final String USER = "postgres";
-    static final String PASS = "sela1234";
 
     public static void connect() throws SQLException {
         Properties props = new Properties();
-        props.setProperty("user", USER);
-        props.setProperty("password", PASS);
+        props.setProperty("user", Configuration.USER);
+        props.setProperty("password", Configuration.PASS);
         Connection conn = DriverManager.getConnection(DB_URL, props);
         System.out.println("connected");
+        Statement statement = conn.createStatement();
+
+        statement.execute("select * from animal");
     }
 }
